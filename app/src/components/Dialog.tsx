8d57b427118fad137d2ed20c9706c380bdb5c869
devilune <devlune@gmail.com>
2020-02-22 19:07:38 +0900

    feat: delete confirmation

---
+++ app/src/components/Dialog.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog as MuiDialog,
+  DialogActions,
+  DialogTitle
+} from "@material-ui/core";
+
+export const useConfirmationDialog = (
+  title: string,
+  confirmActionLabel: string,
+  onConfirm: () => void
+) => {
+  const [open, setOpen] = useState(false);
+
+  const openDialog = () => {
+    setOpen(true);
+  };
+
+  const closeDialog = () => {
+    setOpen(false);
+  };
+
+  const Dialog = () => {
+    return (
+      <MuiDialog open={open} onClose={closeDialog}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogActions>
+          <Button onClick={closeDialog} color="primary">
+            キャンセル
+          </Button>
+          <Button onClick={onConfirm} color="primary" autoFocus>
+            {confirmActionLabel}
+          </Button>
+        </DialogActions>
+      </MuiDialog>
+    );
+  };
+
+  return [Dialog, openDialog, closeDialog];
+};
